df692ec92c241f64609d3f64055770d3ef5bafbb
Abdulla Akhundzada <akhundzada717@gmail.com>
2025-03-03 15:07:05 +0400

    test bugs fixed

--- test.py
+++ test.py
@@ -2,30 +2,32 @@ import unittest
 from nQueens import NQueensSolver
 from utils import is_valid_assignment
 from random import randint
+from time import time
 
-N_MIN, N_MAX = 10, 1000
+N_MIN, N_MAX = 10, 150
+
+def test_case(n):
+    print("\nAssigned N: {}".format(n))
+    st = time() # start time
+    solver = NQueensSolver(n)
+    is_valid = is_valid_assignment(solver.solve())
+    print("\nSolved in {:.3f} seconds.\n".format(time() - st))
+    return is_valid
 
 class TestNQueensSolver(unittest.TestCase):
 
-    def random_test_1(self):
-        solver = NQueensSolver(randint(N_MIN, N_MAX))
-        self.assertFalse(is_valid_assignment(solver.solve()))
+    def test_random_1(self):
+        n = randint(N_MIN, N_MAX)
+        self.assertTrue(test_case(n))
 
-    def random_test_2(self):
-        solver = NQueensSolver(randint(N_MIN, N_MAX))
-        self.assertFalse(is_valid_assignment(solver.solve()))
+    def test_random_2(self):
+        n = randint(N_MIN, N_MAX)
+        self.assertTrue(test_case(n))
     
-    def random_test_3(self):
-        solver = NQueensSolver(randint(N_MIN, N_MAX))
-        self.assertFalse(is_valid_assignment(solver.solve()))
+    def test_random_3(self):
+        n = randint(N_MIN, N_MAX)
+        self.assertTrue(test_case(n))
     
-    def random_test_4(self):
-        solver = NQueensSolver(randint(N_MIN, N_MAX))
-        self.assertFalse(is_valid_assignment(solver.solve()))
-    
-    def random_test_5(self):
-        solver = NQueensSolver(randint(N_MIN, N_MAX))
-        self.assertFalse(is_valid_assignment(solver.solve()))
 
 if __name__ == '__main__':
     unittest.main() 
